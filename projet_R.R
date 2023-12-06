327b72fff70d1fffb81a2ee0418b66d9eeaa25fe
myratoun <ny.rakotomanga@gmail.com>
2023-12-06 21:02:41 +0300

    wip: chore

--- projet_R.R
+++ projet_R.R
@@ -9,4 +9,10 @@ ggplot(df, aes(x=FlightNumber, y=LaunchSite, color=Class)) + geom_point(size=1) 
 ggplot(df, aes(x=PayloadMass, y=LaunchSite, color=Class)) + geom_point(size=1) + scale_color_continuous(low="blue",high="orange")
 
 # Tâche 3
-ggplot(df, aes(x=Orbit, color=Class)) + geom_bar() + scale_color_continuous(low="blue",high="orange")
+ggplot(df, aes(x=Orbit, color=mean(Class))) + geom_bar() + scale_color_continuous(low="blue",high="orange")
+
+# Tâche 4
+ggplot(df, aes(x=FlightNumber, y=Orbit, color=Class)) + geom_point(size=1) + scale_color_continuous(low="blue",high="orange")
+
+# Tâche 5
+ggplot(df, aes(x=PayloadMass, y=Orbit, color=Class)) + geom_point(size=1) + scale_color_continuous(low="blue",high="orange")
